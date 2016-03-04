24ada9a5c62e442686d60c7f762813776a183f82
Lindsay Rispoli <lrispoli2018@pingry.org>
2016-03-03 19:59:18 -0500

    Added a line in the output file that tells the user what the program does

--- tenCharacterLimit.py
+++ tenCharacterLimit.py
@@ -9,11 +9,15 @@ f.close()
 #Open the file output 
 f = open("output.txt", 'w')
 
+#Print a message telling the user what the program did to their input file
+f.write("Some of your lines are too long! They are cut down to 10 characters now!" + "\n")
+
 #loop through the contents and for every line, and write each to the output file
 for currentLine in contents:
 	final = (currentLine[0:10] + "\n") #cuts off the words after ten characters, and new line 	
 	#Write the final message to output 
 	f.write(final) 
+
 
 #Close the output file (outside of the loop)
 f.close()
